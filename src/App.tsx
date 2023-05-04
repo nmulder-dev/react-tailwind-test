0a0aa531cdd6647c6d89f67c686620c98750ec0a
Nick Mulder <nmulder@outlook.com>
2023-05-04 16:38:32 -0500

    testing more flex

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,27 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App flex-auto flex gap-3 ">
-      <div className="border-4 border-black border-solid"></div>
-      <div className="border-4 border-black border-solid w-20 h-20"></div>
-      <div className="border-4 border-black border-solid w-20 h-20"></div>
+    <div>
+      <div
+        id="main"
+        className="border-4 border-black border-solid p-1 flex items-center justify-between"
+      >
+        <div
+          id="flex-1"
+          className="border-4 border-black border-solid p-1 flex-1 text-left"
+        >
+          Flex 1
+        </div>
+        <div id="flex-2" className="border-4 border-black border-solid p-1">
+          Flex 2
+        </div>
+        <div
+          id="flex-3"
+          className="border-4 border-black border-solid p-1 text-center flex-1"
+        >
+          Flex 3
+        </div>
+      </div>
     </div>
   );
 }
